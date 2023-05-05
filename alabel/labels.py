801031afb7b7bb92c26097e39e0239329c82dc08
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-05-06 01:08:31 +0300

    Update labels.py

--- alabel/labels.py
+++ alabel/labels.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-
 """
 
 إضافة تسميات عناصر تصنيفات في ويكي بيانات
 
 نسخ تسمية العنصر إلى التصنيف المطابق له في الإسم
 
+python3 ./core8/pwb.py alabel/labels -limit:20
 
 """
 #
@@ -18,60 +18,66 @@ import pywikibot
 import sys
 #---
 from API import printe
-from api_sql import sql
 from wd_API import himoAPI_test as himoAPI
 #---
 Limit = { 1: '' }
 #---
-def main():
-    #python3 pwb.py wd/labels limit:20
-    #---
-    for arg in sys.argv:
-        arg, sep, value = arg.partition(':')
-        #---
-        if arg == '-limit' or arg == 'limit':
-            Limit[1] = value
-            printe.output('<<lightred>> Limit = %s.' % value )
-        #---#
-    Quaa = '''#USE wikidatawiki_p;
+from api_sql import wiki_sql
+#---
+# result = wiki_sql.sql_new(qua, wiki="", printqua=False)
+#---
+Quaa = '''#USE wikidatawiki_p;
 SELECT
-  CONCAT("Q", ips_item_id),
-  ips_site_page
+    CONCAT("Q", ips_item_id) as qid,
+    ips_site_page as page
 FROM
-  wb_items_per_site
+    wb_items_per_site
 WHERE
-  ips_site_id = 'arwiki'
-  AND NOT EXISTS (
+    ips_site_id = 'arwiki'
+AND NOT EXISTS (
     SELECT
-      wbit_item_id
+        wbit_item_id
     FROM
-      wbt_item_terms
-      INNER JOIN wbt_term_in_lang ON wbtl_id = wbit_term_in_lang_id
-      INNER JOIN wbt_text_in_lang ON wbxl_id = wbtl_text_in_lang_id
+        wbt_item_terms
+        INNER JOIN wbt_term_in_lang ON wbtl_id = wbit_term_in_lang_id
+        INNER JOIN wbt_text_in_lang ON wbxl_id = wbtl_text_in_lang_id
     WHERE
-      wbit_item_id = ips_item_id
-      AND wbxl_language = "ar"
-      AND wbtl_type_id = 1
-  )
+        wbit_item_id = ips_item_id
+        AND wbxl_language = "ar"
+        AND wbtl_type_id = 1
+    )
 '''
+#---
+for arg in sys.argv:
+    arg, sep, value = arg.partition(':')
     #---
-    if Limit[1] != '' :
-        Quaa = Quaa + 'limit %s' % Limit[1]
+    if arg == '-limit' or arg == 'limit':
+        Limit[1] = value
+        printe.output('<<lightred>> Limit = %s.' % value )
+#---
+if Limit[1] != '' :
+    Quaa = Quaa + 'limit %s' % Limit[1]
+#---
+def main():
+    #python3 ./core8/pwb.py alabel/labels -limit:20
     #---
-    printe.output( Quaa )
-    sparql = sql.sparql_generator_url(Quaa)
+    result = wiki_sql.sql_new(Quaa, wiki="wikidata", printqua=True)
     #---
-    Table = {}
-    for item in sparql:
-        Table[item] = sparql[item]
+    len_result = len(result)
     #---
     num = 0
-    for item in Table:
-        num += 1
-        printe.output( '<<lightgreen>> %d/%d item:"%s" ' % (num ,len(Table.keys() ),item) )
-        if Table[item] != "" : 
-            himoAPI.Labels_API( item, Table[item] , "ar" , False, Or_Alii = True)
     #---
+    for item in result:
+        qid  = item['qid']
+        page = item['page']
+        #---
+        num += 1
+        #---
+        printe.output(f'<<lightgreen>> {num}/{len_result} qid:"{qid}", page:"{page}"')
+        #---
+        if page != "" : 
+            himoAPI.Labels_API(qid, page, "ar", False, Or_Alii=True)
+#---
 if __name__ == "__main__":
     main()
 #---
