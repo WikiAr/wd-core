2829e5ce77e164ba5784c8be84c05ba706c446aa
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-07-19 21:52:25 +0300

    u

--- dump/arlanglinks.py
+++ dump/arlanglinks.py
@@ -3,6 +3,7 @@ https://dumps.wikimedia.org/arwiki/20230701/arwiki-20230701-langlinks.sql.gz
 
 python3 core8/pwb.py dump/arlanglinks
 """
+from api_sql import wiki_sql
 import gzip
 import sqlite3
 import sys
@@ -11,11 +12,10 @@ import json
 import pywikibot
 import shutil
 Dump_Dir = os.path.dirname(os.path.realpath(__file__))
-#---
+# ---
 dump_file = f'{Dump_Dir}/langlinks.json'
-#---
-from api_sql import wiki_sql
-#---
+# ---
+# ---
 qua = '''select  
 CONCAT('"Category:', p1.page_title, '"') AS en, CONCAT(':"',ll_title, '",') AS ar
 from page AS p1, langlinks
@@ -24,50 +24,50 @@ AND ll_lang = "ar"
 AND p1.page_namespace = 14
 
 '''
-#---
+# ---
 table = {}
-#---
+# ---
 all = 1000
-#---
+# ---
 TEST = True if 'test' in sys.argv else False
-#---
+# ---
 if TEST:
     all = 20
-#---
+# ---
 offset = 0
-#---
+# ---
 for i in range(1, all):
-    limit   = 200000
-    #---
+    limit = 200000
+    # ---
     if i != 1:
         offset += limit
-    #---
+    # ---
     line = f'limit {limit} offset {offset}'
-    #---
+    # ---
     print(line)
-    #---
+    # ---
     qun = qua
-    #---
+    # ---
     qun += line
-    #---
+    # ---
     if TEST:
         continue
-    #---
+    # ---
     result = wiki_sql.sql_new(qun, wiki="en", printqua=False)
-    #---
-    if not result or len(result) == 0 :
+    # ---
+    if not result or len(result) == 0:
         print('result is empty...')
         break
-    #---
+    # ---
     for x in result:
         en = x['en'].replace('_', ' ')
         ar = x['ar'].replace('_', ' ')
         table[en] = ar
-    #---
+    # ---
     print(f'len of table:{len(table)}')
-    #---
+    # ---
     if TEST:
         break
-    #---
+    # ---
     json.dump(table, open(dump_file, 'w'))
-#---
+# ---
