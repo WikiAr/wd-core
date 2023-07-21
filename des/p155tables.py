26afd78697f809435b44590b691ed9f9be45ccfc
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-07-21 03:55:54 +0300

    Update p155tables.py

--- des/p155tables.py
+++ des/p155tables.py
@@ -12,7 +12,7 @@
 #
 #
 #---
-from make import Sport_key
+from make.lists import Sport_key
 #from make.Sport_key import Sports_Keys_For_Label #Sports_Keys_For_Team#Sports_Keys_For_Jobs#Sports_Keys_For_Label
 #---
 Sports_Keys_Lab = Sport_key.Sports_Keys_For_Label
