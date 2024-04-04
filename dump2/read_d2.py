dce1ef689b15b6bae4e5e413a15b49e6c7600b27
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-04-05 02:10:46 +0300

    Update read_d2.py

--- dump2/read_d2.py
+++ dump2/read_d2.py
@@ -3,7 +3,7 @@ import psutil
 import json
 import sys
 import time
-from pathlip import Path
+from pathlib import Path
 from qwikidata.json_dump import WikidataJsonDump
 def get_most_props():
     # ---
