ba6126f47496caa0b687885ad8d6688262b1db5d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-26 07:49:39 +0300

    Update useraccount.py

--- src/cy/cy_bot/useraccount.py
+++ src/cy/cy_bot/useraccount.py
@@ -4,16 +4,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-qs_token = os.getenv("QS_TOKEN")
-qs_tokenbot = os.getenv("QS_TOKEN_BOT")
+qs_token = os.getenv("QS_TOKEN", "")
+qs_tokenbot = os.getenv("QS_TOKEN_BOT", "")
 
 
 
-username = os.getenv("WIKIPEDIA_BOT_USERNAME")
-password = os.getenv("WIKIPEDIA_BOT_PASSWORD")
+username = os.getenv("WIKIPEDIA_BOT_USERNAME", "")
+password = os.getenv("WIKIPEDIA_BOT_PASSWORD", "")
 
-hiacc = os.getenv("WIKIPEDIA_HIMO_USERNAME")
-hipass = os.getenv("WIKIPEDIA_HIMO_PASSWORD")
+hiacc = os.getenv("WIKIPEDIA_HIMO_USERNAME", "")
+hipass = os.getenv("WIKIPEDIA_HIMO_PASSWORD", "")
 
 if "workibrahem" in sys.argv:
     username = hiacc
