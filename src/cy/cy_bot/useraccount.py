725a17b69a828d0050695828ae79ee93db61582b
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-27 05:11:49 +0300

    Update useraccount.py

--- src/cy/cy_bot/useraccount.py
+++ src/cy/cy_bot/useraccount.py
@@ -1,14 +1,6 @@
 import sys
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
-
-qs_token = os.getenv("QS_TOKEN", "")
-qs_tokenbot = os.getenv("QS_TOKEN_BOT", "")
-
-
-
 username = os.getenv("WIKIPEDIA_BOT_USERNAME", "")
 password = os.getenv("WIKIPEDIA_BOT_PASSWORD", "")
 
