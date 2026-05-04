f6444d8851d48f5d88011b50e234e78168d1df7f
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-04 03:51:36 +0300

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,10 @@ import sys
 from unittest.mock import MagicMock, patch
 
 import pytest
+
+from pytest_socket import disable_socket
+
+
+@pytest.fixture(autouse=True)
+def stop_nets():
+    disable_socket(allow_unix_socket=True)
