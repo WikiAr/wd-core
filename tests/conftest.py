d00d96fceb7172dd5864e9925140ccec7071bdef
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-07 08:05:15 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,5 +9,12 @@ from pytest_socket import disable_socket
 
 
 @pytest.fixture(autouse=True)
-def stop_nets() -> None:
+def stop_nets(request):
+    # Check if 'network' mark is present in the current test item
+    if "network" in request.node.keywords:
+        from pytest_socket import enable_socket
+
+        enable_socket()
+        return
+    # Otherwise, disable the socket for all other tests
     disable_socket(allow_unix_socket=True)
